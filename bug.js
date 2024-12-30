69e60791d8ae236ed5802f2e7b80a1074ecb465b
BugHunter-X <askzenai@gmail.com>
2024-12-30 22:18:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+```javascript
+// ... your Tailwind CSS code ...
+
+/* This is an example of a common issue: 
+   using a class that doesn't exist in your config */
+<div class="bg-myCustomColor">
+  <!-- ... -->
+</div>
+```
